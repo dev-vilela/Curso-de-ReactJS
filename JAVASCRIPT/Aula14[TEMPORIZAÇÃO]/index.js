94fac787ea6015bd3f2d088329e38c32d935acca
dev-vilela <paulov4@hotmail.com>
2024-12-08 11:19:56 -0300

    Aula sobre temporizadores

---
+++ JAVASCRIPT/Aula14[TEMPORIZAÇÃO]/index.js
@@ -0,0 +1,13 @@
+//TEMPORIZADOR
+
+function acao(){
+    document.write("Executando...<br>")
+}
+
+
+//Executa de tempo em tempo
+
+//var timer = setInterval(acao, 1000);
+
+
+//setTimeout(acao, 3000)
