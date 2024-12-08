bed2edf2d86cd33b934e4215dcb78ab52a88454d
dev-vilela <paulov4@hotmail.com>
2024-12-08 10:53:43 -0300

    Aula 12 sobre Laços de repetição LOOPS

---
+++ JAVASCRIPT/Aula12[LOOPS]/index.js
@@ -0,0 +1,18 @@
+// WHILE = ENQUANTO
+
+x = 0;
+
+while(x < 10){
+    document.write("<br> O valor do X: " + x);
+
+    x++; // x = x + 1
+}
+document.write("<br><br> <br>  O X esta valendo: " + x + "<br> <br>");
+
+/* */
+//FOR = PARA
+
+for(a = 0; a < 10; a++){
+    document.write("<br> O valor do A: " + a);
+
+}
