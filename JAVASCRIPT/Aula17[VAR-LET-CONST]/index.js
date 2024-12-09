c3a702e94df4208fb2d8f1652da132cde48a4d6c
dev-vilela <paulov4@hotmail.com>
2024-12-09 19:43:02 -0300

    Aula sobre as variaveis VAR, LET e CONST

---
+++ JAVASCRIPT/Aula17[VAR-LET-CONST]/index.js
@@ -0,0 +1,35 @@
+/* ---------------VAR, LET E CONST-------------------
+
+
+EXEMPLO 01:
+var nome = "Paulo";
+
+if(nome == "Paulo"){
+    var idade = 23;
+
+    console.log("Tem " + idade)
+}
+    ----------------------------
+EXEMPLO 02:
+var nome = "Paulo";
+
+if(nome == "Paulo"){
+    let sobrenome = "Vilela";
+
+    console.log("Paulo " + sobrenome)
+}
+ */
+
+var lista = [1,2,3];
+
+for(var i in lista){
+    console.log(lista[i]);
+}
+
+const nome = "Paulo";
+console.log(nome)
+nome = "VileLA"
+
+
+
+
