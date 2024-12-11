4927c39bc95b6d4f286eef25dc6b409e35ab7859
dev-vilela <paulov4@hotmail.com>
2024-12-11 19:39:58 -0300

    INCLUDES,ENDSWITH E STARTSWITH

---
+++ JAVASCRIPT/Aula24[INCLUDES,ENDSWITH E STARTSWITH]/index.js
@@ -0,0 +1,35 @@
+/*---------------[INCLUDES,ENDSWITH E STARTSWITH]----------------------
+
+EXEMPLO 01
+
+
+*/
+
+
+//INCLUDES - VERIFICA 
+/*
+let nomes = ['Paulo', 'Jader', 'Saulo'];
+
+console.log(nomes.includes('Saulo'))
+
+if(nomes.includes('Jader')){
+    console.log("Usuario cadastrado!")
+}else{
+    console.log("Usuario não encontrado.")
+}
+ */
+//----------------------------------
+
+//ENDSWITH E ENDSWITH
+
+let nomes = "Paulo";
+
+console.log(nomes.endsWith("o")) //TERMINA COM TAL COISA EX: LETRA
+
+console.log(nomes.endsWith("o")) //COMEÇA COM TAL COISA EX: LETRA
+
+
+
+
+
+
