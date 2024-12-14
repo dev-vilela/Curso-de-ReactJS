28e8b9d24e7fbed33782d1040b31c92da41424c9
dev-vilela <paulov4@hotmail.com>
2024-12-13 23:18:12 -0300

    aula sobre react, avanços com os estudos

--- REACT/meuapp/src/App.js
+++ REACT/meuapp/src/App.js
@@ -1,8 +1,23 @@
-export default function App(){
-  return(
+import React from "react";
+
+const Bemvindo = (props) => {
+  return (
     <div>
-      <h1>Bem Vindo ao sistema</h1>
-      <h2>teste ;-)</h2>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Sua senha é: {props.senha}</h3>
     </div>
   )
 }
+
+function App(){
+  return(
+    <div>
+      Olá Mundo!
+      <Bemvindo nome="Paulo" senha="0430201" />
+      <Bemvindo nome="Vilela" senha="4924032" />
+      <h1>Curso de React</h1>
+    </div>
+  );
+}
+
+export default App;
