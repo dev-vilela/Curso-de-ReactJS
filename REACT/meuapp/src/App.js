07f3f145209552302dad1e7a2f3b756d89a01901
dev-vilela <paulov4@hotmail.com>
2024-12-24 08:41:29 -0300

    Aula 36 sobre STATE e fechamento do modulo 4

--- REACT/meuapp/src/App.js
+++ REACT/meuapp/src/App.js
@@ -51,8 +51,8 @@ function App(){
 
 export default App;
 
-*/
-
+====================================================
+AULA 35:
 import React, { Component } from "react";
 
 class Equipe extends Component{
@@ -89,3 +89,61 @@ function App(){
 }
 
 export default App;
+
+*/
+//TRABALHANDO COM STATE
+import React, {Component} from 'react';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome:'Paulo',
+      contador: 0
+    };
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this)
+  }
+
+  aumentar(){
+    let state = this.state;
+    state.contador +=1
+    state.nome = 'Wende'
+    this.setState(state)
+  }
+
+  diminuir(){
+    let state = this.state;
+    if(state.contador === 0){
+      alert('Chegou a zero!');
+        return;
+    }
+    state.contador -= 1;
+    state.nome = 'Paulo'
+    this.setState(state)
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Contador</h1>
+
+          {this.state.nome}
+
+         <h3>
+          
+          <button onClick={this.diminuir}>-</button> 
+               { this.state.contador }
+          <button onClick={this.aumentar}>+</button>
+
+          </h3>
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
