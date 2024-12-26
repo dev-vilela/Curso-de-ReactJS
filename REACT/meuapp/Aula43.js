0a0aa592430af4a6d42d836d0f82a1f3bd88cdc5
dev-vilela <paulov4@hotmail.com>
2024-12-26 08:25:08 -0300

    Aula 43 manipulando formularios.

---
+++ REACT/meuapp/Aula43.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class Aula43 extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Aula43
--- REACT/meuapp/src/App.js
+++ REACT/meuapp/src/App.js
@@ -235,9 +235,8 @@ class App extends Component{
 }
 
 export default App;
-
-*/
-
+----------------------------------------------------
+AULA 42:
 
 import React, {Component} from 'react';
 import Feed from './aula42/Feed';
@@ -272,4 +271,55 @@ class App extends Component{
 }
 
 export default App;
+*/
 
+
+import React, {Component} from "react";
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            senha: '',
+            sexo:'masculino'
+        }
+
+        this.trocaEmail = this.trocaEmail.bind(this);
+        this.trocaSexo = this.trocaSexo.bind(this)
+    }
+
+    trocaEmail(e){
+      let valorDigitado = e.target.value;
+      this.setState({email:valorDigitado});
+    }
+
+    trocaSexo(e){
+      let valorDigitado = e.target.value;
+      this.setState({sexo: valorDigitado});
+    }
+
+    render(){
+        return(
+            <div>
+             <h2>Login</h2>
+                Email:
+                <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail} /> <br/> <br/>
+                Senha:
+                <input type="text" name="senha" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} /> <br/> <br/>
+                Sexo:
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo} >
+                    <option value="masculino" >Masculino</option>
+                    <option value="feminino" >Feminino</option>
+                </select>
+
+                <div>
+                  <h3>{this.state.email}</h3>
+                  <h3>{this.state.senha}</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App
