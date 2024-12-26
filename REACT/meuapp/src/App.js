b3ab0ae7b5c7c3b259070e9b380f9f80cc894991
dev-vilela <paulov4@hotmail.com>
2024-12-26 08:50:13 -0300

    Aula 44, criando um formulario com eventos para alertar se tiver campos vazios

--- REACT/meuapp/src/App.js
+++ REACT/meuapp/src/App.js
@@ -271,9 +271,8 @@ class App extends Component{
 }
 
 export default App;
-*/
-
-
+---------------------------------------------------------
+AULA 43:
 import React, {Component} from "react";
 
 class App extends Component{
@@ -323,3 +322,61 @@ class App extends Component{
 }
 
 export default App
+*/
+
+
+import React, {Component} from "react";
+
+class App extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = {
+        nome:'',
+        email:'',
+        senha: '',
+        error: ''
+      };
+
+      this.cadastrar = this.cadastrar.bind(this);
+    }
+
+    cadastrar(event){
+      const{nome, email, senha} = this.state;
+
+      if(nome !== '' && email !== '' && senha !== '' ){
+        alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
+      }else{
+        this.setState({error: 'Ops! Ficou faltando algum campo.'})
+      }
+
+      
+
+      event.preventDefault()
+    }
+   
+    render(){
+        return(
+            <div>
+              <h1>Novo usuario</h1>
+
+              {this.state.error && <p>{this.state.error}</p>}
+
+              <form onSubmit={this.cadastrar} >
+                <label>Nome:</label>
+                <input type="text" value={this.state.name} onChange={(e) => this.setState({nome: e.target.value})} /> <br/> <br/>
+
+                <label>Email:</label>
+                <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})} /> <br/> <br/>
+
+                <label>Senha:</label>
+                <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})} /> <br/> <br/>
+
+                <button type="submit" >Cadastrar</button>
+              </form>
+            </div>
+        );
+    }
+}
+
+export default App;
