88a7610f80ca8150ca8e7a4a893ebd471ab472ff
dev-vilela <paulov4@hotmail.com>
2025-01-01 19:31:01 -0300

    Projeto em finalização, criado a pagina home, detalhes falta ainda o css

--- REACT/modulo_8/src/pages/Home/index.js
+++ REACT/modulo_8/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import {useEffect , useState} from 'react';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
+import './home.css'
 //
 
 function Home(){
 
-        const [filmes, setFilmes] = ([]);
+        const [filmes, setFilmes] = useState([]);
+        const [loading, setLoading] = useState(true);
 
         useEffect(() =>{
             
@@ -17,16 +20,37 @@ function Home(){
                     }
                 })
 
-                console.log(response.data.results);
+                // console.log(response.data.results.slice(0, 10));
+                setFilmes(response.data.results.slice(0, 10));
+                setLoading(false);
             };
 
             loadFilmes();
 
         },[])
 
+        if(loading){
+            return(
+                <div className='loading' >
+                    <h1>Carregando filmes...</h1>
+                </div>
+            );
+        }
+
     return(
-        <div>
-            <h1>PAGINA HOME</h1>
+        <div className='container' >
+            <div className='lista-filmes' >
+                {filmes.map((filmes) => {
+                    return(
+                        <article key={filmes.id} >
+                            <strong>{filmes.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filmes.poster_path}` }  alt={filmes.title} />
+                            <Link to={`/filme/${filmes.id}`} >Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
+            
         </div>
     );
 }
