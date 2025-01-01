7846b67299bd8b865fb909010526db581f421155
dev-vilela <paulov4@hotmail.com>
2025-01-01 20:46:51 -0300

    feita a parte de detalhes dos filmes

--- REACT/modulo_8/src/pages/Filme/index.js
+++ REACT/modulo_8/src/pages/Filme/index.js
@@ -1,7 +1,9 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-
+import './filme-info.css';
 import api from '../../services/api'
+import {Link} from 'react-router-dom';
+
 
 function Filme(){
         const {id} = useParams();
@@ -48,9 +50,22 @@ function Filme(){
             <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}` }  alt={filme.title} />
 
             <h2>Sinopse</h2>
-            <spna>{filme.overview}</spna>
+            <span>{filme.overview}</span>
 
             <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-btn' >
+                <button>Salvar</button>
+                <button>
+                    <a href='#' >Trailer</a>
+                </button>
+
+                <Link to='/' className='btn-voltar' >Voltar</Link>
+            </div>
+
+            {/* <div className='btn-voltar' >
+            <Link to='/' >Voltar</Link>
+            </div> */}
         </div>
     );
 }
