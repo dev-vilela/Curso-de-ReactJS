f0caebbd457daebf7bfb86c8b444dbbfdcfd85cb
dev-vilela <paulov4@hotmail.com>
2025-01-02 23:05:41 -0300

    Projeto finalizado

---
+++ REACT/modulo_8/src/pages/Favoritos/index.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+function Favoritos (){
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() =>{
+
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || []);
+
+
+    },[])
+
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item) => {
+                return(item.id !== id )
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+        toast.success("FILME REMOVIDO COM SUCESSO!")
+    }
+
+    return(
+        <div className='meus-filmes' >
+            <h1>Minha Lista</h1>
+
+            {filmes.length === 0 && <span>Nada encontrado na lista!</span>}
+
+            <ul>
+                {filmes.map((item) =>{
+                    return(
+                        <li key={item.id} >
+                            <span>{item.title}</span>
+
+                            <div>
+                                <Link to={`/filme/${item.id}`} >Ver detalhes</Link>
+                                <button onClick={() => excluirFilme(item.id)} >Excluir</button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default Favoritos;
--- REACT/modulo_8/src/pages/Filme/index.js
+++ REACT/modulo_8/src/pages/Filme/index.js
@@ -1,12 +1,13 @@
 import {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api'
 import {Link} from 'react-router-dom';
-
+import { toast } from 'react-toastify';
 
 function Filme(){
         const {id} = useParams();
+        const navigation = useNavigate();
         const [filme, setFilme] = useState({});
         const  [loading, setLoading] = useState(true);
 
@@ -21,10 +22,12 @@ function Filme(){
 
                 .then((response) =>{
                     setFilme(response.data);
-                    setLoading(false);
+                    setLoading(false)
                 })
                 .catch(()=>{
                     console.log("Filme não encontrado")
+                    navigation("/", {replace: true})
+                    return;
                 })
             }
 
@@ -33,7 +36,26 @@ function Filme(){
             return() =>{
                 console.log("COMPONETE FOI DESMONTADO")
             }
-        }, []);
+        }, [navigation, id]);
+
+        function salvarFilme(){
+            const minhaLista = localStorage.getItem("@primeflix");
+
+            let filmesSalvos = JSON.parse(minhaLista) || [];
+
+            const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id )
+
+            if(hasFilme){
+                toast.warn("ESSE FILME JÁ ESTA LISTA")
+                return;
+            }
+
+            filmesSalvos.push(filme);
+            localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+            toast.success("Filme salvo com sucesso!")
+
+
+        }
 
         if(loading){
             return(
@@ -55,9 +77,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-btn' >
-                <button>Salvar</button>
+                <button onClick={salvarFilme} >Salvar</button>
                 <button>
-                    <a href='#' >Trailer</a>
+                    <a target="blank" rell="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer `  } >Trailer</a>
                 </button>
 
                 <Link to='/' className='btn-voltar' >Voltar</Link>
